a8e5250f3be04d498270e842731e4d2cbf6a39c0
unknown <antonydamico29@gmail.com>
2018-09-30 14:32:52 -0400

    creating movieList views

--- movieList/views.py
+++ movieList/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# pylint: disable=E1101
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import Movie
+
+class MovieListView(generic.ListView):
+    queryset = Movie.objects.all()
+    template_name = 'movieList/list.html'
+    context_object_name = 'movies'
+
+class MovietDetailSlugView(generic.DetailView):
+    queryset = Movie.objects.all()
+    template_name = 'posts/detail.html'
+    context_object_name = 'post'
