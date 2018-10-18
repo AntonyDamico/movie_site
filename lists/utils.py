6796d95ba2d3f8a16b1f9bbdf7702a65aec058a8
unknown <antonydamico29@gmail.com>
2018-10-18 11:32:37 -0400

    finishing utils in lists

--- lists/utils.py
+++ lists/utils.py
@@ -4,6 +4,6 @@ def format_movie(qs_movie):
     new_title = qs_movie.get('title')
     new_year = qs_movie.get('year')
     new_poster = qs_movie.get('poster')
-    new_movie = Movie(title=new_title, year)
+    new_movie = Movie(title=new_title, year=new_year, poster=new_poster)
     print('new movie', new_movie)
     return new_movie
