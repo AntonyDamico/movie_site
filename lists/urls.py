b4fcbd6ee082e37e9926d529905dece38245d0d4
unknown <antonydamico29@gmail.com>
2018-10-18 09:18:41 -0400

    creating delete movie from list and profiles

--- lists/urls.py
+++ lists/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'list'
 
 urlpatterns = [
-    path('', views.movie_list_view, name='index')
+    path('', views.movie_list_view, name='index'),
+    path('<int:movie_id>/delete', views.delete_movie_from_list, name='delete_movie')
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 from .models import List
 from movies.models import Movie
@@ -10,3 +11,5 @@ def movie_list_view(request):
     context = {'movies': user_movies}
     return render(request, 'lists/list.html', context)
 
+def delete_movie_from_list(request, movie_id):
+    return HttpResponse('I\'m working on it')
