759162d76c46188cbeda06b73b0e146b5512d162
Antony D'Amico <antonydamico29@gmail.com>
2018-10-19 16:57:03 -0400

    formating

--- ratings/models.py
+++ ratings/models.py
@@ -9,6 +9,7 @@ class RatingManager(models.Manager):
         new_movie_rating = Rating(movie=movie, rating=rating)
         new_movie_rating.save()
 
+
 class Rating(models.Model):
     movie = models.OneToOneField(Movie, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(default=0)
@@ -18,8 +19,10 @@ class Rating(models.Model):
     def __str__(self):
         return f'{self.movie} rating is {self.rating}'
 
+
 class UserRating(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             blank=True, null=True, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, unique=True, on_delete=models.CASCADE)
     user_rating = models.PositiveIntegerField(default=0)
 
