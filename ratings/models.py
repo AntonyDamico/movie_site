a8c85afb5d132120c5aa147ca9baee0ef54ecaa8
Antony D'Amico <antonydamico29@gmail.com>
2018-10-19 17:23:44 -0400

    more formating

--- ratings/models.py
+++ ratings/models.py
@@ -13,11 +13,14 @@ class Rating(models.Model):
     def __str__(self):
         return f'{self.movie} rating is {self.rating}'
 
+
 def add_rating_to_movie_post_reciever(sender, instance, created, *args, **kwargs):
     if instance and created:
         Rating.objects.get_or_create(movie=instance)
 
+
 post_save.connect(add_rating_to_movie_post_reciever, sender=Movie)
+
 
 class UserRating(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
