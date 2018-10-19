6b4e47a70b4f3f1c5e96b7b8dea4f82ef26234f7
Antony D'Amico <antonydamico29@gmail.com>
2018-10-19 16:19:23 -0400

    creating rating and userrating models

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from movies.models import Movie
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, unique=True, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(default=None)
+
+    def __str__(self):
+        return f'{self.movie} rating is {self.rating}'
+
+class UserRating(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, unique=True, on_delete=models.CASCADE)
+    user_rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.user} rating for {self.movie} is {self.user_rating}'
