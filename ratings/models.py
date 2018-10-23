d0a532a54ace09d73da31cec21d22d185acda72b
Antony D'Amico <antonydamico29@gmail.com>
2018-10-23 18:42:54 -0400

    adding user rating manager

--- ratings/models.py
+++ ratings/models.py
@@ -22,11 +22,21 @@ def add_rating_to_movie_post_reciever(sender, instance, created, *args, **kwargs
 post_save.connect(add_rating_to_movie_post_reciever, sender=Movie)
 
 
+class UserRatingManager(models.Manager):
+    def rate_new_movie(self, user, movie, rating):
+        new_rating = self(user, movie, rating)
+        new_rating.save()
+        rating_obj = movie.rating
+        rating_obj.rating = (rating_obj + rating)/2
+        rating_obj.save()
+
 class UserRating(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              blank=True, null=True, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, unique=True, on_delete=models.CASCADE)
     user_rating = models.PositiveIntegerField(default=0)
+
+    objects = UserRatingManager()
 
     def __str__(self):
         return f'{self.user} rating for {self.movie} is {self.user_rating}'
