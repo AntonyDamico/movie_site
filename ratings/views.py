c6b9b4cece861df12dd477378297c575a85a5ab2
Antony D'Amico <antonydamico29@gmail.com>
2018-10-23 19:04:37 -0400

    creating rate_movie_view

--- ratings/views.py
+++ ratings/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import UserRating
+from movies.models import Movie
 
 def rate_movie_view(request, movie_id):
-    pass
+    user = request.user
+    user_rating = request.POST.get('user_rating')
+    movie = get_object_or_404(Movie, pk=movie_id)
+    UserRating.objects.rate_new_movie(user, movie, user_rating)
+    return redirect('list:index')
+
