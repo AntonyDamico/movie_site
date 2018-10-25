9fa60065494b1bb887f15c513dd7e4c8c259a027
Antony D'Amico <antonydamico29@gmail.com>
2018-10-25 11:30:55 -0400

    hiding secret key

--- movieSiteDjango/settings.py
+++ movieSiteDjango/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from env.secret_key import SECRET_KEY_PERSONAL
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qb=+-gw0bcng52+uqn(y#m48od@8z@0(ex%ob+)f%q!u(i5)vj'
+SECRET_KEY = SECRET_KEY_PERSONAL
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
