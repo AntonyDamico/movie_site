17a04342627ac388b3fbb94301f36c07be67aeec
Antony D'Amico <antonydamico29@gmail.com>
2018-10-30 14:03:23 -0400

    adding movie serializer

---
+++ movies/serializers.py
@@ -0,0 +1,9 @@
+# pylint: disable=E1101
+from rest_framework import serializers
+from .models import Movie
+
+
+class MovieSerializer(serializers.ModelSerialiizer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'slug', 'poster', 'year')
